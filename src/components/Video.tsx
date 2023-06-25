6a99c8ba0538ee549a865294c6b33e0332f4268f
Eden <eden.reich@gmail.com>
2023-06-25 23:39:54 +0200

    Add loading skeleton while waiting for the video to be there

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import type VideoProps from '@/types/Video';
 import dynamic from 'next/dynamic';
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, Suspense } from 'react';
+import Loading from './Loading';
 
-const ReactPlayer = dynamic(() => import('react-player/youtube'), { ssr: false });
+const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
 
 const Video: React.FC<VideoProps> = ({ url, title, date, excerpt }) => {
   const videoWrapperRef = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLDivElement>;
@@ -38,24 +41,26 @@ const Video: React.FC<VideoProps> = ({ url, title, date, excerpt }) => {
   }, []);
 
   return (
-    <div className="bg-white rounded-lg shadow-lg md:p-2 lg:p-4 mt-4">
-      <div ref={videoWrapperRef} className="video-container">
-        <ReactPlayer
-          playing={isVisible && screenWidth < 768}
-          ref={videoRef}
-          url={url}
-          title={title}
-          className="w-full"
-          controls={true}
-          width="100%"
-          height="116vh"
-        />
-      </div>
-      <div className="p-4">
-        <h3 className="text-xl font-bold mb-2">{title}</h3>
-        <p className="text-gray-600 text-sm mb-2">{date}</p>
-        <p className="text-gray-700 text-base">{excerpt}</p>
-      </div>
+    <div ref={videoWrapperRef} className="bg-white rounded-lg shadow-lg md:p-2 lg:p-4 mt-4">
+      <Suspense fallback={<Loading />}>
+        <div className="video-container">
+          <ReactPlayer
+            playing={isVisible && screenWidth < 768}
+            ref={videoRef}
+            url={url}
+            title={title}
+            className="w-full"
+            controls={true}
+            width="100%"
+            height="116vh"
+          />
+        </div>
+        <div className="p-4">
+          <h3 className="text-xl font-bold mb-2">{title}</h3>
+          <p className="text-gray-600 text-sm mb-2">{date}</p>
+          <p className="text-gray-700 text-base">{excerpt}</p>
+        </div>
+      </Suspense>
     </div>
   );
 };
