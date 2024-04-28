652be30c3c7ee40e5b90583b485fc3c1ab3063b8
Eden Reich <eden.reich@gmail.com>
2024-04-28 20:10:13 +0200

    Add Roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import '@/styles/globals.css';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
